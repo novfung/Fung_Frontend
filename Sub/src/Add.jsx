d8b44d7485b77cbf53369fa0feb47a4d9ac547d2
tccmw <jungmwoo6248@gmail.com>
2024-11-17 23:18:23 +0900

    게시물 삭제 기능 구현

--- Sub/src/Add.jsx
+++ Sub/src/Add.jsx
@@ -1,69 +1,72 @@
-import axios from "axios";
-import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+  import axios from "axios";
+  import { useRef,useState } from "react";
+  import { Link, useNavigate, useLocation } from "react-router-dom";
 
-function Add() {
-  const [form, setForm] = useState({
-    title:"",
-    content:"",
-  });
+  function Add() {
+    const [form, setForm] = useState({
+      title: "",
+      content: "",
+      key:1
+    });
 
-  const handleChange = e =>{
-    const {name, value} = e.target
-    setForm((prev) => ({
-      ...prev,
-      [name] : value,
-    }))
-  }
+    const handleChange = (e) => {
+      const { name, value } = e.target;
+      setForm((prev) => ({
+        ...prev,
+        [name]: value,
+      }));
+    };
 
-  const nav = useNavigate()
-
-  const AddButton = () =>{
-    if(form.title !== "" && form.content !== ""){
-      axios({
-        method: "post",
-        url: `http://localhost:8080/feed`,
-        data: {
-          title: form.title,
-          content: form.content,
-          // image_url: "string",
-        },
-      })
-      .then((result) => {
-        alert("추가")
-        console.log(result.data)
-        nav("/LM",{state :{form:form}});
-      })
-      .catch((error) => {
-        alert("실패")
-        console.log(error)
-      })
-    }else{
-      alert("입력해")
-    }
-  }
-  return (
-    <div>
+    const nav = useNavigate();
+    
+    const AddButton = () => {
+      if (form.title !== "" && form.content !== "") {
+        const accessToken = localStorage.getItem("accessToken");
+        const feeds = JSON.parse(localStorage.getItem("feeds")) || []
+        const newKey = feeds.length > 0 ? Math.max(...feeds.map(feed => feed.key))+1:1
+        const newFeed = {...form, key: newKey}
+        axios
+          .post(`http://3.36.53.67:8080/feed`, JSON.stringify(newFeed), {
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${accessToken}`,
+            },
+          })
+          .then((res) => {
+            console.log("성공", res);
+            feeds.push(newFeed)
+            nav("/LM");
+            localStorage.setItem("feeds",JSON.stringify(feeds))
+          })
+          .catch((error) => {
+            console.log("실패", error);
+          });
+        }else{
+          console.log('모든 필드를 입력하세요')
+        }
+    };
+    return (
       <div>
-        <input 
-        placeholder="제목" 
-        type="text" 
-        name="title" 
-        onChange={handleChange}/>
-        <input 
-        placeholder="내용" 
-        type="text" 
-        name="content" 
-        onChange={handleChange}/>
-      </div>
-      <div>
+        <input
+          placeholder="제목"
+          type="text"
+          name="title"
+          value={form.title}
+          onChange={handleChange}
+        />
+        <input
+          placeholder="내용"
+          type="text"
+          name="content"
+          value={form.content}
+          onChange={handleChange}
+        />
         <Link to="/LM" style={{ textDecoration: "none" }}>
           <button>취소</button>
         </Link>
         <button onClick={AddButton}>추가</button>
       </div>
-    </div>
-  );
-}
+    );
+  }
 
-export default Add;
+  export default Add;
--- Sub/src/LoginMain.jsx
+++ Sub/src/LoginMain.jsx
@@ -1,21 +1,48 @@
-import { Link, useNavigate } from "react-router-dom";
-import { useState } from "react";
-import axios from "axios";
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
-function LoginMain(){
-  return(
+function LoginMain() {
+  const [feeds, setFeeds] = useState([]);
+  useEffect(() => {
+    const JSonData = JSON.parse(localStorage.getItem("feeds")) || [];
+    setFeeds(JSonData);
+  }, []);
+
+  const onDataDelete = (key) => {
+    const notDelete = feeds.filter((item)=> item.key !== key)
+    localStorage.setItem('feeds',JSON.stringify(notDelete))
+    setFeeds(notDelete)
+    alert("필드를 삭제합니다")
+  };
+
+  return (
     <div>
-      <div>
-        <Link to='/MyPage' style={{textDecoration : 'none'}}>
-          <button>마이 페이지</button>
-        </Link>
-        <Link to='/Add' style={{textDecoration : 'none'}}>
-          <button>추가</button>
-        </Link>
-        <Link to="/AddMain" style={{textDecoration:'none'}}>
-          <div>게시물</div>
-        </Link>
-      </div>
+      <Link to="/MyPage" style={{ textDecoration: "none" }}>
+        <button>마이 페이지</button>
+      </Link>
+      <Link to="/Add" style={{ textDecoration: "none" }}>
+        <button>추가</button>
+      </Link>
+
+      {feeds.length === 0 ? (
+        <p>등록된 것이 없다</p>
+      ) : (
+        <ul>
+          {feeds.map((feed, index) => {
+            return (
+              <div key={feed.key}>
+                <Link to="/AddMain" style={{ textDecoration: "none" }}>
+                  <li>
+                    <h3>{feed.title}</h3>
+                    <p>{feed.content}</p>
+                  </li>
+                </Link>
+                <div onClick={() => {onDataDelete(feed.key);}}>삭제</div>
+              </div>
+            );
+          })}
+        </ul>
+      )}
     </div>
   );
 }
